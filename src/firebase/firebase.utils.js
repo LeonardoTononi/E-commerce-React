5d3f40f848381424a9cdcfb8f5353d75d5df6500
Leonardo Tononi <easaaa@protonmail.com>
2019-08-04 18:28:20 +0200

    Added createUserPofileDocument in firebase file and logic auth in componentDidMount

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -7,12 +7,37 @@ const config = {
   authDomain: 'crwn-commerce-db.firebaseapp.com',
   databaseURL: 'https://crwn-commerce-db.firebaseio.com',
   projectId: 'crwn-commerce-db',
-  storageBucket: '',
+  storageBucket: 'crwn-commerce-db.appspot.com',
   messagingSenderId: '753625557228',
   appId: '1:753625557228:web:6781a2a375e0b6c7'
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
